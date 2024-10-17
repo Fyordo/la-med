c17615cc5836dd73ec095be5e8f258ebd98472eb
Fedor <lagutinfedya@gmail.com>
2024-10-17 19:31:40 +0300

    build: init project

---
+++ src/main/kotlin/fyordo/lifeagragator/med/MedApplication.kt
@@ -0,0 +1,11 @@
+package fyordo.lifeagragator.med
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class MedApplication
+
+fun main(args: Array<String>) {
+	runApplication<MedApplication>(*args)
+}
---
+++ src/test/kotlin/fyordo/lifeagragator/med/MedApplicationTests.kt
@@ -0,0 +1,13 @@
+package fyordo.lifeagragator.med
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class MedApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
